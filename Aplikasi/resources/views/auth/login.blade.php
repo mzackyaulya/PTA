72e032c2b7984d66f01cefa59335631342567c30
mzackyaulya <muhammadzackyaulya_2226240096@mhs.mdp.ac.id>
2025-09-14 14:36:54 +0700

    update

--- Aplikasi/resources/views/auth/login.blade.php
+++ Aplikasi/resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login Akademikl</title>
+  <title>Login Akademik</title>
   <link rel="shortcut icon" type="image/png" href="{{ url('assets/img/logoweb.png') }}" />
   <link rel="stylesheet" href="{{ url('assets/css/styles.min.css') }}" />
 </head>
@@ -13,16 +13,17 @@
 <body>
     <!--  Body Wrapper -->
     <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full" data-sidebar-position="fixed" data-header-position="fixed">
-        <div class="position-relative overflow-hidden text-bg-light min-vh-100 d-flex align-items-center justify-content-center">
+        <div class="position-relative overflow-hidden text-bg-light min-vh-100 d-flex align-items-center justify-content-center bg-success">
             <div class="d-flex align-items-center justify-content-center w-100">
                 <div class="row justify-content-center w-100">
                     <div class="col-md-8 col-lg-6 col-xxl-3">
                         <div class="card mb-0">
                             <div class="card-body">
                                 <a href="/" class="text-nowrap logo-img text-center d-block py-3 w-50">
-                                    <img src="{{ url('assets/img/Logo.png') }}" alt="" width="300px" height="120px">
+                                    <img src="{{ url('assets/img/Logo.png') }}" alt="" width="305px" height="120px">
                                 </a>
-                                <p class="text-center">Selamat Datang di Akademik Sma Muhammadiyah 2 Palembang</p>
+                                <p class="text-center">Selamat Datang di Akademik</p>
+                                <p class="text-center">Sma Muhammadiyah 2 Palembang</p>
                                 <form method="POST" action="{{ route('login') }}">
                                     <div class="mb-3">
                                         <label for="exampleInputEmail1" class="form-label">Username</label>
